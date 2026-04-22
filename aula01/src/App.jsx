263e347c3969c31ea938dd7c6bbe81dd0d2d9008
joao marcelo <joaomarcelooliveira5000@gmail.com>
2026-04-22 14:57:44 -0300

    22/04

---
+++ aula01/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+export default function App()
+{
+
+  let nome, idade, dias;
+  nome = "João Marcelo";
+  idade = 16;
+  dias = idade * 365;
+  return(
+    <div>
+
+    <h1>Olá Mundo !!!</h1>
+
+    <div className="conteudo">
+
+    <p>Olá, seja bem vindo ao react com js.</p>
+
+    </div>
+
+    </div>
+
+  )
+}
